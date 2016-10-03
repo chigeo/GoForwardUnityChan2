617810c4c1a5c742495f60156bb84e88ff1b8796
chigeo <misasagi.119@gmail.com>
2016-10-03 22:29:23 +0900

    ２D横スクロールアクションゲーム
    授業内で製作した成果物

---
+++ Assets/CubeController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeController : MonoBehaviour {
+
+	// キューブの移動速度
+	private float speed = -0.2f;
+
+	// 消滅位置
+	private float deadLine = -10;
+
+	// Use this for initialization
+	void Start(){
+	}
+
+	// Update is called once per frame
+	void Update () {
+		// キューブを移動させる
+		transform.Translate (this.speed, 0, 0);
+
+		// 画面外に出たら破棄する
+		if (transform.position.x < this.deadLine){
+			Destroy (gameObject);
+		}
+	}
+
+	// キューブが何かに接触した
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		// キューブが地面に接触する時と
+		// キューブ同士が積み重なるときに効果音をならす
+		if (other.transform.tag == "GroundTag" || other.transform.tag == "CubeTag")
+		{
+			GetComponent<AudioSource>().Play();
+		}
+	}
+}
